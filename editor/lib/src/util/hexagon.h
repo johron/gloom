02a894104b4dd188ff13a46b9be66799d9c81a2a
Unknown <johan.ronnkvist@gmail.com>
2018-07-28 21:51:30 +0200

    corrected hexagon position calculation

--- editor/lib/src/util/hexagon.h
+++ editor/lib/src/util/hexagon.h
@@ -13,3 +13,23 @@ namespace util {
 				{ position.x() + size * cos(qDegreesToRadians(330.0)), position.y() + size * sin(qDegreesToRadians(330.0)) } }) { }
 	};
 }
+
+namespace gloom {
+	using hex_coordinate = std::tuple<int, int, int>;
+	inline QPointF to_pos(hex_coordinate c, float size) {
+		const auto w = sqrt(3) * size;
+		const auto h = size * 2;
+
+		const auto y = std::get<2>(c) * h * 3/4;
+		const auto x = std::get<0>(c) * w/2 + std::get<1>(c) * -w/2;
+		return { x, y };
+	}
+
+
+	struct hexagon : public QGraphicsPolygonItem {
+		hexagon(hex_coordinate coordinate, float size, QGraphicsItem* parent)
+			: QGraphicsPolygonItem(util::hexagon(to_pos(coordinate, size), size), parent) {
+			setPen(QPen(Qt::red, 4));
+		}
+	};
+}
